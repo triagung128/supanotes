f20d9809fb64f467e7087f19a5be7f2c1e802474
triagung128 <triagung128@gmail.com>
2022-09-06 00:59:31 +0700

    setting supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+import 'app/controllers/auth_controller.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Supabase supabase = await Supabase.initialize(
+    url: 'https://oxqyygxnjowludvxqdon.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94cXl5Z3huam93bHVkdnhxZG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzNjk1MDksImV4cCI6MTk3Nzk0NTUwOX0.hXK017mpaEI7NxgTqjWfFWBvM3r28m-R0Imd9lKjS8c',
+  );
+
+  if (kDebugMode) {
+    print(supabase.client.auth.session());
+  }
+
+  Get.put(AuthController(), permanent: true);
+
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Supanotes",
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPages.initial,
+      initialRoute:
+          supabase.client.auth.currentUser == null ? Routes.login : Routes.home,
       getPages: AppPages.routes,
     ),
   );
