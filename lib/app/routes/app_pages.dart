0d480fcc6d5fd91b33e045e4fdea47a4e4800a5b
triagung128 <triagung128@gmail.com>
2022-09-06 01:00:56 +0700

    setting pages route

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,55 @@
 import 'package:get/get.dart';
 
+import '../modules/add_note/bindings/add_note_binding.dart';
+import '../modules/add_note/views/add_note_view.dart';
+import '../modules/edit_note/bindings/edit_note_binding.dart';
+import '../modules/edit_note/views/edit_note_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
+import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const initial = Routes.home;
+  static const initial = Routes.login;
 
   static final routes = [
     GetPage(
       name: _Paths.home,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.login,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.register,
+      page: () => const RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.profile,
+      page: () => ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.addNote,
+      page: () => AddNoteView(),
+      binding: AddNoteBinding(),
+    ),
+    GetPage(
+      name: _Paths.editNote,
+      page: () => EditNoteView(),
+      binding: EditNoteBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,19 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const home = _Paths.home;
+  static const login = _Paths.login;
+  static const register = _Paths.register;
+  static const profile = _Paths.profile;
+  static const addNote = _Paths.addNote;
+  static const editNote = _Paths.editNote;
 }
 
 abstract class _Paths {
   _Paths._();
   static const home = '/home';
+  static const login = '/login';
+  static const register = '/register';
+  static const profile = '/profile';
+  static const addNote = '/add-note';
+  static const editNote = '/edit-note';
 }
