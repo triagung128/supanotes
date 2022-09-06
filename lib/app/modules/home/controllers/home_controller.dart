90c0ccd95e854d5d9e4c148f711f9ad7a2227594
triagung128 <triagung128@gmail.com>
2022-09-06 10:38:08 +0700

    feat: list_note

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,31 @@
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class HomeController extends GetxController {}
+import '../../../data/models/note_model.dart';
+
+class HomeController extends GetxController {
+  RxList allNotes = List<Note>.empty().obs;
+
+  final SupabaseClient _client = Supabase.instance.client;
+
+  Future<void> getAllNotes() async {
+    PostgrestResponse<dynamic> user =
+        await _client.from('users').select('id').match({
+      'uid': _client.auth.currentUser!.id,
+    }).execute();
+
+    int id = (user.data as List).first['id'];
+
+    var notes = await _client.from('notes').select().match({
+      'user_id': id,
+    }).execute();
+    List<Note> dataNote = Note.fromJsonList(notes.data as List);
+    allNotes(dataNote);
+    allNotes.refresh();
+  }
+
+  void deleteNote(int id) async {
+    await _client.from('notes').delete().match({'id': id}).execute();
+    await getAllNotes();
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,66 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import '../../../routes/app_pages.dart';
+import '../../../data/models/note_model.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Home'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => Get.toNamed(Routes.profile),
+            icon: const Icon(Icons.person),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: FutureBuilder(
+        future: controller.getAllNotes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Obx(
+            () => controller.allNotes.isEmpty
+                ? const Center(
+                    child: Text('Data is empty'),
+                  )
+                : ListView.builder(
+                    itemCount: controller.allNotes.length,
+                    itemBuilder: (context, index) {
+                      Note note = controller.allNotes[index];
+                      return ListTile(
+                        onTap: () => Get.toNamed(
+                          Routes.editNote,
+                          arguments: note,
+                        ),
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text('${note.title}'),
+                        subtitle: Text('${note.description}'),
+                        trailing: IconButton(
+                          onPressed: () => controller.deleteNote(note.id!),
+                          icon: const Icon(Icons.delete),
+                        ),
+                      );
+                    },
+                  ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.toNamed(Routes.addNote),
+        child: const Icon(Icons.add),
       ),
     );
   }
